b12604fa15086dba5699453289ccd66cf12a278d
Anton Aksionau <ikari808@gmail.com>
2023-07-10 15:27:40 +0200

    fix copy icon margin

--- src/components/ComfyPrompt.vue
+++ src/components/ComfyPrompt.vue
@@ -150,7 +150,7 @@ function handleCopyGroup(prompt) {
               @click="handleCopyGroup(prompt)"
               class="px-2.5 py-2.5 inline-flex items-center text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
-              <IconCopy class="mr-0" />
+              <IconCopy style="margin: 0" />
             </button>
           </div>
         </div>
