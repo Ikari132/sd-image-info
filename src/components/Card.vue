f6ac3ce6f53afebcc0c3a434e385c31ad8311f36
Anton Aksionau <ikari808@gmail.com>
2023-07-10 19:10:54 +0200

    add card component

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+  url: {
+    type: String,
+  },
+});
+</script>
+<template>
+  <div
+    class="hover:saturate-150 hover:drop-shadow-md max-w-xs bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+  >
+    <a href="#">
+      <img class="rounded-lg" :src="props.url" alt="" />
+    </a>
+  </div>
+</template>
