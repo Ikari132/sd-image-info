31be2aae1c47c3b8e47fd628869ea6a298ff33ef
Anton Aksionau <ikari808@gmail.com>
2023-09-14 00:15:53 +0200

    fix buttons margin

--- src/components/DefaultPrompt.vue
+++ src/components/DefaultPrompt.vue
@@ -78,7 +78,7 @@ function handleCopyForTextbox() {
       class="inline-flex items-center py-2.5 px-3 sm:mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
       title="clear"
     >
-      <IconCancel class="mr-0" />
+      <IconCancel style="margin: 0" />
     </button>
     <button
       @click="showSaveModal = true"
@@ -111,7 +111,7 @@ function handleCopyForTextbox() {
           class="inline-flex items-center py-2.5 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
           title="close"
         >
-          <IconCancel class="mr-0" />
+          <IconCancel style="margin: 0" />
         </button>
         <button
           @click="handleSave"
